c1fa2e3d1b99d6a0c526a182e39536b0b16c13e8
Gustavo Borda <30397715+guhborda@users.noreply.github.com>
2018-08-10 05:39:00 -0300

    Some initial files

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+
+if(isset($_SESSION['user'])){
+}else{
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Bankeger Landing Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+    <script src="node_modules/jquery/dist/jquery.js"></script>
+    <script src="js/core/popper.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+    <!--Bootstrap Plugins-->
+    <script src="js/plugins/bootstrap-notify.js"></script>
+    <script src="js/plugins/bootstrap-select.min.js"></script>
+    <script src="js/plugins/bootstrap-switch.min.js"></script>
+    <script src="js/plugins/bootstrap-tagsinput.min.js"></script>
+    <script src="js/plugins/chartjs.min.js"></script>
+    <script src="js/plugins/jquery.validate.js"></script>
+    <script src="js/plugins/perfect-scrollbar.jquery.min.js"></script>  
+    <script src="js/index.js"></script>
+</head>
+<body>
+    <div class="section">
+    </div>
+</body>
+</html>
